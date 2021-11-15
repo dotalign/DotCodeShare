7c76d10e76aa3801340f54fcbb5abb03434c5a46
Jaspreet Bakshi <jaspreet@dotalign.com>
2021-11-15 15:14:10 -0500

    Updated getdomaintable

--- SQL/GetDomainTable.sql
+++ SQL/GetDomainTable.sql
@@ -27,8 +27,7 @@ BEGIN
 	set @countOfDots = LEN(@hostName) - LEN(REPLACE(@hostName, '.', ''))
 
 	-- if the domain is like dotalign.com.au or dotalign.co.uk
-	if (((PARSENAME(@hostNameWithSubdomains, 2) = 'co' OR PARSENAME(@hostNameWithSubdomains, 2) = 'com')) 
-			AND (@countOfDots = 2))
+	if (((PARSENAME(@hostNameWithSubdomains, 2) = 'co' OR PARSENAME(@hostNameWithSubdomains, 2) = 'com')) AND (@countOfDots = 2))
 		SELECT @hostName = @hostNameWithSubdomains
 	ELSE 
 		SELECT @hostName = PARSENAME(@hostNameWithSubdomains, 2) + '.' + PARSENAME(@hostNameWithSubdomains, 1)
@@ -36,4 +35,35 @@ BEGIN
 	INSERT INTO @result SELECT @hostName, @hostNameWithSubdomains, @url
 	RETURN
 END
-GO
+
+-- TESTS
+-- select * from dbo.GetDomainTable('https://www.google.com/hello')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://www.google.com/hello')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://post.in/instructions')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://julio.co.uk')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://us.tradeshow.com')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://post.in/instructions')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://julio.co.uk')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://us.tradeshow.com')
+-- UNION 
+-- select * from dbo.GetDomainTable('https://dotalign.com.au/#team')
+-- UNION 
+-- select * from dbo.GetDomainTable('http://dotalign.com.in/contact-us')
+
