88d1ade6b41599ffe06ece8499d343a7eda34740
Jaspreet Bakshi <jaspreet@dotalign.com>
2021-11-18 00:39:36 -0500

    Updated the GetDomainTable function to also support SLDs

--- SQL/GetDomainTable.sql
+++ SQL/GetDomainTable.sql
@@ -33,15 +33,33 @@ BEGIN
 	DECLARE @hostNameWithSubdomains varchar(512)
 	SET @hostNameWithSubdomains = @hostName
 
-	DECLARE @countOfDots int;
-	SET @countOfDots = LEN(@hostName) - LEN(REPLACE(@hostName, '.', ''))
+	-- Special handling for second level domains like dotalign.com.au or dotalign.co.uk
+	-- More info can be found here https://en.wikipedia.org/wiki/Second-level_domain
+	DECLARE @secondLevelDomains TABLE (DomainName VARCHAR(20))
+		
+	-- List of second level domain names we're handling
+	INSERT INTO @secondLevelDomains(DomainName) 
+	VALUES	('co'), 
+			('com'), 
+			('firm'),
+			('net'),
+			('org'), 
+			('gen'), 
+			('ernet'), 
+			('ac'), 
+			('edu'), 
+			('res'), 
+			('gov'), 
+			('mil')
 
-	-- if the domain is like dotalign.com.au or dotalign.co.uk
-	if (((PARSENAME(@hostNameWithSubdomains, 2) = 'co' OR PARSENAME(@hostNameWithSubdomains, 2) = 'com')) 
-			AND (@countOfDots = 2))
-		SELECT @hostName = @hostNameWithSubdomains
-	ELSE 
-		SELECT @hostName = PARSENAME(@hostNameWithSubdomains, 2) + '.' + PARSENAME(@hostNameWithSubdomains, 1)
+	DECLARE @sld VARCHAR(20)
+	set @sld = PARSENAME(@hostName, 2)
+
+	-- If the second level domain exists in the list
+	IF EXISTS (SELECT * FROM @secondLevelDomains WHERE DomainName = @sld)
+		SELECT @hostName = PARSENAME(@hostName, 3) + '.' + PARSENAME(@hostName, 2) + '.' + PARSENAME(@hostName, 1)				
+	ELSE
+		SELECT @hostName = PARSENAME(@hostName, 2) + '.' + PARSENAME(@hostName, 1)
 
 	INSERT INTO @result SELECT @hostName, @hostNameWithSubdomains, @url
 	RETURN
@@ -50,33 +68,42 @@ END
 -- TESTS
 -- =====
 
--- SELECT * from dbo.GetDomainTable('https://www.google.com/hello')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://www.google.com/hello')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://post.in/instructions')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://julio.co.uk')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://us.tradeshow.com')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://post.in/instructions')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://julio.co.uk')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://us.tradeshow.com')
--- UNION 
--- SELECT * from dbo.GetDomainTable('https://dotalign.com.au/#team')
--- UNION 
--- SELECT * from dbo.GetDomainTable('http://dotalign.com.in/contact-us')
-
+--SELECT * from dbo.GetDomainTable('www.nescol.ac.uk')
+--UNION
+--SELECT * from dbo.GetDomainTable('https://books.gosimplebooks.co.uk/')
+--UNION
+--SELECT * from dbo.GetDomainTable('http://www.bas.com.bh/')
+--UNION
+--SELECT * from dbo.GetDomainTable('https://business.bankofscotland.co.uk/')
+--UNION
+--SELECT * from dbo.GetDomainTable('https://practices.healthengine.com.au')
+--UNION
+--SELECT * from dbo.GetDomainTable('https://www.google.com/hello')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://www.google.com/hello')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://post.in/instructions')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://julio.co.uk')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://us.tradeshow.com')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://help.dotalign.com/article/5wwii9q2b4-migrating-from-adal-to-msal')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://dotalign--c.documentforce.com/secur/contentDoor?startURL=https%3A%2F%2FdskipRedirect=1&lm=eyJlbmMiOiJBMjU2R0NNIijowfQ%3D%3D..TbgpNG20jF_KWTa6.Gx5Rjlh9cad9mgX0UQWYVw%3D%3D.bWYNdVsK1zeqCEd-8c8sOg%3D%3D')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://post.in/instructions')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://julio.co.uk')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://us.tradeshow.com')
+--UNION 
+--SELECT * from dbo.GetDomainTable('https://dotalign.com.au/#team')
+--UNION 
+--SELECT * from dbo.GetDomainTable('http://dotalign.com.in/contact-us')
