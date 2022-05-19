1bb2dfc41d010d814a924c35cf0544b24802352e
Jaspreet Bakshi <jaspreet@dotalign.com>
2022-05-19 12:37:33 -0400

    Master - fixed team number

--- SQL/Views/ColleagueEmail.sql
+++ SQL/Views/ColleagueEmail.sql
@@ -1,13 +1,13 @@
--- DROP VIEW IF EXISTS da.team1_colleague_email;
+-- DROP VIEW IF EXISTS da.team2_colleague_email;
 
-CREATE VIEW da.team1_colleague_email
+CREATE VIEW da.team2_colleague_email
 AS
 SELECT DISTINCT 
   coll.colleague_id,
   ema2.email_address_id,
   ema2.email_address_text
-FROM da.team1_colleague coll
-JOIN da.team1_contact_email_address ema ON ema.email_address_text = coll.email_address
-JOIN da.team1_contact_identity_map map ON map.secondary_id = ema.email_address_id
-JOIN da.team1_contact_identity_map map2 ON map2.primary_id = map.primary_id AND map2.id != map.id
-JOIN da.team1_contact_email_address ema2 ON ema2.contact_id = map2.secondary_id;
+FROM da.team2_colleague coll
+JOIN da.team2_contact_email_address ema ON ema.email_address_text = coll.email_address
+JOIN da.team2_contact_identity_map map ON map.secondary_id = ema.email_address_id
+JOIN da.team2_contact_identity_map map2 ON map2.primary_id = map.primary_id AND map2.id != map.id
+JOIN da.team2_contact_email_address ema2 ON ema2.contact_id = map2.secondary_id;
--- SQL/Views/ContactMeetingStat.sql
+++ SQL/Views/ContactMeetingStat.sql
@@ -7,10 +7,10 @@ SELECT  collema.colleague_id,
         COUNT(DISTINCT mtg.meeting_id) AS NumMeetings,
         MIN(mtg.start_date) AS FirstMeeting,
         MAX(mtg.start_date) AS LastMeeting
-FROM da.team1_colleague_email collema
+FROM da.team2_colleague_email collema
 JOIN da.meeting_participant collpar ON collpar.email_address = collema.email_address_text
 JOIN da.meeting mtg ON mtg.meeting_id = collpar.meeting_id
 JOIN da.meeting_participant contpar ON contpar.meeting_id = mtg.meeting_id AND contpar.id != collpar.id
-JOIN da.team1_contact_email_address contema ON contema.email_address_text = contpar.email_address
-JOIN da.team1_contact cont ON cont.contact_id = contema.contact_id
+JOIN da.team2_contact_email_address contema ON contema.email_address_text = contpar.email_address
+JOIN da.team2_contact cont ON cont.contact_id = contema.contact_id
 GROUP BY collema.colleague_id, cont.contact_id;
