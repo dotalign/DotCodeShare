01fd99ecdfe6a188d37c3b5916a9520da2e56c1d
Iryna Chechko <iryna.obertynska@litslink.com>
2025-03-17 09:29:13 +0200

    fix(Sh-1): fix the query selector name

--- Marlin/loadInDotAlign.js
+++ Marlin/loadInDotAlign.js
@@ -1,21 +1,21 @@
 const loadInDotAlign = (relativePath) => {
     const iFrame = document.querySelector('#dotalignEmbedApp')
-    
+
     if (!iFrame) {
       console.error('The DotAlign iFrame was not found');
       return;
     }
-  
+
     const { origin } = new URL(iFrame.src);
 
     if (iFrame.src === `${origin}/blank.html` || iFrame.src.indexOf('options.html') > -1) {
-      const parentNode = document.querySelector('.dotalign-iFrame')
+      const parentNode = document.querySelector('.dotalign-iframe')
       const newIFrame = iFrame.cloneNode()
       newIFrame.src = `${origin}/${relativePath}`
       parentNode.replaceChild(newIFrame, iFrame)
     } else {
       iFrame.contentWindow.postMessage(`url:/${relativePath}`, iFrame.src)
     }
-    
+
     window.postMessage('da_open', '*');
 }
