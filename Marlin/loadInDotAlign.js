b47cef9afc03c15ccee45b4e40ea721dce3ec168
Iryna Chechko <iryna.obertynska@litslink.com>
2025-03-17 09:34:49 +0200

    feat(Sh-1): encode url

--- Marlin/loadInDotAlign.js
+++ Marlin/loadInDotAlign.js
@@ -11,10 +11,10 @@ const loadInDotAlign = (relativePath) => {
     if (iFrame.src === `${origin}/blank.html` || iFrame.src.indexOf('options.html') > -1) {
       const parentNode = document.querySelector('.dotalign-iframe')
       const newIFrame = iFrame.cloneNode()
-      newIFrame.src = `${origin}/${relativePath}`
+      newIFrame.src = `${origin}/${encodeURI(relativePath)}`
       parentNode.replaceChild(newIFrame, iFrame)
     } else {
-      iFrame.contentWindow.postMessage(`url:/${relativePath}`, iFrame.src)
+      iFrame.contentWindow.postMessage(`url:/${encodeURI(relativePath)}`, iFrame.src)
     }
 
     window.postMessage('da_open', '*');
